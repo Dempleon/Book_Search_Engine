5162f8c4ae959605254972367eceee71c77c0226
Dempoleon <dempoleon@gmail.com>
2022-12-08 20:31:05 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/search-books', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
