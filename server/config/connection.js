0c2d865d7c85174a635e519565322831fb3e291f
Dempoleon <dempoleon@gmail.com>
2022-12-08 20:33:35 -0800

    changed mongodb uri to deploy again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/search-books', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/search-books', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
